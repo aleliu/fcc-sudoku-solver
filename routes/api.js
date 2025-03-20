5469ee458a999b76d8fc2e93f864b1c82cca344a
aleliu <aleliug@gmail.com>
2025-03-20 17:36:09 -0400

    feat: check api

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,37 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-
+      let row, col, rowChecker, colChecker, areaChecker, result;
+      const solver = new SudokuSolver();
+      let value = req.body.value;
+      let puzzle = req.body.puzzle;
+      [row, col] = req.body.coordinate.split('');
+      row = "abcdefghi".indexOf(row.toLowerCase());
+      col = Number(col);
+      if (row == -1){
+        return res.json({ "error": "Invalid coordinate" });
+      } else {
+        row += 1;
+      }
+      if ( col < 1 || col > 9) return res.json({ "error": "Invalid coordinate" });
+      rowChecker = solver.checkRowPlacement(puzzle, row, col, value);
+      colChecker = solver.checkColPlacement(puzzle, row, col, value);
+      areaChecker = solver.checkRegionPlacement(puzzle, row, col, value);
+      if (rowChecker && colChecker && areaChecker){
+        return res.json({valid: true})
+      } else {
+        result = {valid: false, conflict: []};
+        if (!rowChecker) {
+          result.conflict.push('row')
+        }
+        if (!colChecker) {
+          result.conflict.push('column')
+        }
+        if (!areaChecker) {
+          result.conflict.push('region')
+        }
+        return res.json(result);
+      }
     });
     
   app.route('/api/solve')

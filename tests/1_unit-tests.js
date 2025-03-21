3d3bb8636303ad5af164c731836472d4390ad6ba
aleliu <aleliug@gmail.com>
2025-03-20 22:42:38 -0400

    fix: import puzzles

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,14 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-const puzzles = require('../controllers/puzzle-strings.js');
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js');
 const { stat } = require('@babel/core/lib/gensync-utils/fs.js');
 let solver = new Solver();
 
 suite('Unit Tests', () => {
     test('Logic handles a valid puzzle string of 81 characters', (done) => {
-        let solution = solver.solve(puzzles[0][0]);
-        assert.equal(solution, puzzles[0][1]);
+        let solution = solver.solve(puzzlesAndSolutions[0][0]);
+        assert.equal(solution, puzzlesAndSolutions[0][1]);
         done();
     });
     test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', (done) => {
@@ -18,53 +18,53 @@ suite('Unit Tests', () => {
         done();
     });
     test('Logic handles a puzzle string that is not 81 characters in length', (done) => {
-        let solution = solver.solve(puzzles[0][0]+"2");
+        let solution = solver.solve(puzzlesAndSolutions[0][0]+"2");
         assert.equal(solution, "Expected puzzle to be 81 characters long");
         done();
     });
     test('Logic handles a valid row placement', (done) => {
-        let solution = solver.checkRowPlacement(puzzles[0][0], "B", 2, 4);
+        let solution = solver.checkRowPlacement(puzzlesAndSolutions[0][0], "B", 2, 4);
         assert.deepEqual(solution, { "valid": true });
         done();
     }); 
     test('Logic handles an invalid row placement', (done) => {
-        let solution = solver.checkRowPlacement(puzzles[0][0], "B", 2, 3);
+        let solution = solver.checkRowPlacement(puzzlesAndSolutions[0][0], "B", 2, 3);
         assert.deepEqual(solution, { "valid": false, "conflict": [ "row" ] });
         done();
     });
     test('Logic handles a valid column placement', (done) => {
-        let solution = solver.checkColPlacement(puzzles[0][0], "B", 2, 4);
+        let solution = solver.checkColPlacement(puzzlesAndSolutions[0][0], "B", 2, 4);
         assert.deepEqual(solution, { "valid": true});
         done();
     });
     test('Logic handles an invalid column placement', (done) => {
-        let solution = solver.checkColPlacement(puzzles[0][0], "B", 2, 9);
+        let solution = solver.checkColPlacement(puzzlesAndSolutions[0][0], "B", 2, 9);
         assert.deepEqual(solution, { "valid": false, "conflict": [ "column" ] });
         done();
     });
     test('Logic handles a valid region (3x3 grid) placement', (done) => {
-        let solution = solver.checkRegionPlacement(puzzles[0][0], "B", 2, 4);
+        let solution = solver.checkRegionPlacement(puzzlesAndSolutions[0][0], "B", 2, 4);
         assert.deepEqual(solution, { "valid": true });
         done();
     });
     test('Logic handles an invalid region (3x3 grid) placement', (done) => {
-        let solution = solver.checkRegionPlacement(puzzles[0][0], "B", 2, 5);
+        let solution = solver.checkRegionPlacement(puzzlesAndSolutions[0][0], "B", 2, 5);
         assert.deepEqual(solution, { "valid": false, "conflict": [ "area" ] });
         done();
     });
     test('Valid puzzle strings pass the solver', (done) => {
-        let solution = solver.solve(puzzles[0][0]);
-        assert.deepEqual(solution, puzzles[0][1]);
+        let solution = solver.solve(puzzlesAndSolutions[0][0]);
+        assert.deepEqual(solution, puzzlesAndSolutions[0][1]);
         done();
     });
     test('Invalid puzzle strings fail the solver', (done) => {
-        let solution = solver.solve(puzzles[0][0]+"2");
+        let solution = solver.solve(puzzlesAndSolutions[0][0]+"2");
         assert.deepEqual(solution, { "error": "Expected puzzle to be 81 characters long" });
         done();
     });
     test('Solver returns the expected solution for an incomplete puzzle', (done) => {
-        let solution = solver.solve(puzzles[1][0]);
-        assert.equal(solution, puzzles[1][1]);
+        let solution = solver.solve(puzzlesAndSolutions[1][0]);
+        assert.equal(solution, puzzlesAndSolutions[1][1]);
         done();
     });
 });
